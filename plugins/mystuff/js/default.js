e477f38f93d05b861629e856b74e0ffc38d5b8b3
Mahdi P <pedramphp@gmail.com>
2012-11-09 14:08:01 -0800

    fixing $ issue

--- plugins/mystuff/js/default.js
+++ plugins/mystuff/js/default.js
@@ -161,7 +161,7 @@
 					if(totalPrice > viPrice ){
 						$viPrice.parents(".u-cb").after($("<div class='ebay-genie-vi-price' title='Sell your stuff to get it free' style='clear:both; margin-left: 80px; font-size: 13px; font-weight: bold; color: green;'>Get it for FREE</div>"));
 					}else{
-						buyPrice = "$" + addCommas(parseFloat(viPrice - totalPrice).toFixed(2)).toFixed(2);
+						buyPrice = "$" + addCommas(parseFloat(viPrice - totalPrice).toFixed(2));
 						log("buy price", buyPrice);
 						$viPrice.parents(".u-cb").after($("<div class='ebay-genie-vi-price' title='Sell your stuff to get this price' style='clear:both; margin-left: 80px; font-size: 13px; font-weight: bold; color: green;'>Get it for "+buyPrice+"</div>"));	
 					}
@@ -179,7 +179,8 @@
 						if(totalPrice > viPrice ){
 							$viPrice.parents("#actualPriceRow").after($("<tr class='ebay-genie-vi-price' title='Sell your stuff to get it free'><td class='priceBlockLabelPrice' id='actualPriceLabel'>Price: </td><td style='clear:both; margin-left: 80px; font-size: 13px; font-weight: bold; color: green;'>Get it for FREE<td></tr>"));
 						}else{
-							buyPrice = "$" + addCommas(viPrice - totalPrice);
+							buyPrice = "$" + addCommas(parseFloat(viPrice - totalPrice).toFixed(2));
+							log("buy price", buyPrice);
 							$viPrice.parents("#actualPriceRow").after($("<tr class='ebay-genie-vi-price' title='Sell your stuff to get this price' ><td class='priceBlockLabelPrice' id='actualPriceLabel'>Price: </td><td style='clear:both; margin-left: 80px; font-size: 13px; font-weight: bold; color: green;'>Get it for " + buyPrice + "<td></tr>"));	
 						}
 					}
